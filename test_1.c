d74617a090597d46e395b3a7cfa8fb0abcbc646d
Y14 <91432431+Y1414@users.noreply.github.com>
2024-03-22 12:54:07 +0100

    Dodano plik do testów

---
+++ test_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "ff.h"
+#include "read.h"
+
+int main (){
+    FILE* in = fopen("300_20.txt", "r+");
+    int start=0, end=0, width=0, len=0;
+    read(&start, &end, &width, in);
+
+    
+    while(ff(in, width));
+;
+    fclose(in);
+
+    return 0;
+}
